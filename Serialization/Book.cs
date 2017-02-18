e8ad4cd3c268a17d630690142a0fa8cbceb3d141
Valiantsin Lopan <valiantsin_lopan@live.com>
2017-02-18 22:10:35 +0300

    Book and Catalog classes created

---
+++ Serialization/Book.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Book
+    {
+        private DateTime publishDate;
+        private DateTime registationDate;
+
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+        [XmlAttribute("isbn")]
+        public string Isbn { get; set; }
+
+        [XmlAttribute("author")]
+        public string Author { get; set; }
+
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+        [XmlElement("genre")]
+        public Genre Genre { get; set; }
+
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("publish_date")]
+        public string PublishDate
+        {
+            get
+            {
+                return publishDate.ToString("yyyy-MM-dd");
+            }
+            set
+            {
+                publishDate= DateTime.Parse(value);
+            }
+        }
+
+        [XmlElement("registration_date")]
+        public string RegistrationDate
+        {
+            get
+            {
+                return registationDate.ToString("yyyy-MM-dd");
+            }
+            set
+            {
+                registationDate = DateTime.Parse(value);
+            }
+        }
+        public Book()
+        {
+
+        }
+
+    }
+    public enum Genre
+    {
+        Computer,
+        Fantasy,
+        Romance,
+        Horror,
+
+        [XmlEnum("Science Fiction")]
+        ScienceFiction
+    }
+}
---
+++ Serialization/Catalog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Catalog
+    {
+        private DateTime date;
+        [XmlElement("book")]
+        public List<Book> Books { get; set; }
+
+        [XmlAttribute()]
+        public string Date
+        {
+            get { return Convert.ToDateTime(date).ToString("yyy-MM-dd"); }
+            set { date = DateTime.Parse(value); }
+
+        }
+        public Catalog()
+        {
+            Books = new List<Book>();
+        }
+
+        public Catalog(IEnumerable<Book> books)
+        {
+            Books = books.ToList();
+        }
+    }
+}
